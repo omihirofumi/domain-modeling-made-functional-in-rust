f5b93758c73bb24af8869cd39bdb22cce53d3d04
omihirofumi <99390907+omihirofumi@users.noreply.github.com>
2026-04-06 20:13:45 +0900

    re init

--- README.md
+++
@@ -1,2 +0,0 @@
-# domain-modeling-made-functional-in-rust
-書籍「関数型ドメインモデリング」をRustで学ぶやつ
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-mod place_order;
-
-use domain_modeling_made_functional_in_rust::types;
-
 fn main() {
-    let v = types::String50::new("".to_string()).unwrap();
+    println!("Hello, world!");
 }
